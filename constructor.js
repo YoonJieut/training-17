77b5685b1e04e2b11fb5a2ab8f0c34a47da60e85
YoonJunHyeon <qmdldos@gmail.com>
2023-10-23 19:51:06 +0900

    메인 머지 후, makeObjectOne 생성, 매개변수로, 객체를 리턴하는 함수

--- constructor.js
+++ constructor.js
@@ -1,1 +1,19 @@
-console.log('hi');
+const makeObjectOne = (idValue, pwValue, emailValue) => {
+  return {
+    id : idValue,
+    password : pwValue,
+    email : emailValue
+  }
+};
+
+// 리턴데이터 타입을 객체로 만들고, "정적인" key 값에
+// "동적인" value 값을 매개변수를 통해 만들었다.
+// "매개변수 인자를 객체에 전달했다"라고 표현했다.
+
+
+// 함수 makeObjectOne()은 객체를 리턴했기 때문에,
+// result 변수는 사실상 객체를 담은 변수이다.
+
+let result = makeObjectOne("아이디테스트", "pw 테스트", "email 테스트");
+console.log(result);
+//{ id: '아이디테스트', password: 'pw 테스트', email: 'email 테스트' }
