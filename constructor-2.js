a1617325fe54b87b3551737e3e5501694500bb52
YoonJunHyeon <qmdldos@gmail.com>
2023-10-23 20:07:37 +0900

    테스트 완료, 첫 번째와의 차이는 '인스턴스'를 갖는 것

--- constructor-2.js
+++ constructor-2.js
@@ -6,3 +6,15 @@ function makeObjectTwo(idValue, pwValue, emailValue) {
   this.email = emailValue;
 }
 
+// 함수를 호출할 때 new라는 키워드를 추가했다.
+let result = new makeObjectTwo('아이디테스트', '패스워드','이메일');
+
+// 결과적으로 변수 result는 객체이지만, 
+// '무엇인가 함수로부터 만들어진' '인스턴스(instance)'라고 명명한다.
+
+// 예제 -1번과 결과물은 같은 객체이기 때문에 같아보이지만,
+// '인스턴스'이냐 유무에서 완전히 다른 판정을 내린다.
+
+console.log(result);
+//makeObjectTwo { id: '아이디테스트', password: '패스워드', email: '이메일' }
+// 객체 명이 담긴 인스턴스를 가지고 있다.
